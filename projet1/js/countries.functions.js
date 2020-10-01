d62e90176bf7acc1cb8103a0a94e5e3ff3234fbe
woozyfr <alex@woozy.fr>
2020-10-01 21:18:05 +0200

    add other version of project1

---
+++ projet1/js/countries.functions.js
@@ -0,0 +1,61 @@
+function filterCountries(country) {
+    const all_countries = document.querySelectorAll('[data-filter-block]');
+    for (let i = 0; i < all_countries.length; i++) {
+        console.log(all_countries[i].dataset.filterBlock);
+        if (all_countries[i].dataset.filterBlock === country) {
+            all_countries[i].style.display = "flex";
+        } else {
+            all_countries[i].style.display = "none";
+        }
+    }
+}
+
+
+
+
+const cards = document.querySelector(".article-section");
+
+// Creation d'une base template pour une card
+function template_coutrny_card(obj) {
+
+    const card = document.createElement("div");
+    card.classList.add("article-bloc");
+    card.classList.add("article-bloc-"+obj.category_key);
+    card.classList.add("article-bloc-"+obj.category_key);
+    card.dataset.filterBlock = obj.category_key;
+
+    cards.appendChild(card);
+
+    // Gestion de l'image
+    const cardPictures = document.createElement("div");
+    card.appendChild(cardPictures);
+
+    const images = document.createElement("img");
+    images.setAttribute('src', obj.picture);
+    cardPictures.appendChild(images);
+
+    // Gestion du contenu
+    const cardContent = document.createElement("div");
+    card.appendChild(cardContent);
+
+    const cardContentTitle = document.createElement("h2");
+    cardContentTitle.textContent = obj.name;
+    cardContent.appendChild(cardContentTitle);
+
+    const cardContentTitleTags = document.createElement("span");
+    cardContentTitleTags.classList.add("tags");
+    cardContentTitleTags.classList.add("tags-"+obj.category_key);
+    cardContentTitleTags.textContent = obj.category_name;
+    cardContentTitle.appendChild(cardContentTitleTags);
+
+    const cardContentText = document.createElement("p");
+    cardContentText.textContent = obj.text;
+    cardContent.appendChild(cardContentText);
+
+    const cardContentLink = document.createElement("a");
+    cardContentLink.textContent = "En voir plus";
+    cardContentLink.href = obj.url;
+    cardContentLink.classList.add("btn");
+    cardContentLink.classList.add("btn-default");
+    cardContent.appendChild(cardContentLink);
+}
---
+++ projet1/js/database/countries.js
@@ -0,0 +1,69 @@
+const countriesDatabase = [{
+    "name": "Thailand",
+    "category_name": "Asia",
+    "category_key": "asia",
+    "url": "country.html?p=thailand",
+    "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni iure sunt cum ullam facilis quae. Totam, nulla, porro asperiores numquam atque unde maiores quidem earum, dicta cupiditate dolorum harum eius?",
+    "picture": "images/thailand-1400x788-1.jpg"
+    },
+    {
+        "name": "Chine",
+        "category_name": "Asia",
+        "category_key": "asia",
+        "url": "country.html?p=chine",
+        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni iure sunt cum ullam facilis quae. Totam, nulla, porro asperiores numquam atque unde maiores quidem earum, dicta cupiditate dolorum harum eius?",
+        "picture": "images/sejour-linguistique-chine.jpg"
+
+    },
+    {
+        "name": "Inde",
+        "category_name": "Asia",
+        "category_key": "asia",
+        "url": "country.html?p=india",
+        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni iure sunt cum ullam facilis quae. Totam, nulla, porro asperiores numquam atque unde maiores quidem earum, dicta cupiditate dolorum harum eius?",
+        "picture": "images/india-1370586.jpg"
+
+    },
+    {
+        "name": "Pérou",
+        "category_name": "Amerique",
+        "category_key": "america",
+        "url": "country.html?p=thailand",
+        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni iure sunt cum ullam facilis quae. Totam, nulla, porro asperiores numquam atque unde maiores quidem earum, dicta cupiditate dolorum harum eius?",
+        "picture": "images/image_processing20180916-4-ezfmel.jpg"
+
+    },
+
+    {
+        "name": "Argentine",
+        "category_name": "Amerique",
+        "category_key": "america",
+        "url": "country.html?p=argentine",
+        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni iure sunt cum ullam facilis quae. Totam, nulla, porro asperiores numquam atque unde maiores quidem earum, dicta cupiditate dolorum harum eius?",
+        "picture": "images/glacier-perito-moreno-parc-national-los-glaciares-patagonie-argentine-609021-1440x720.jpg"
+
+    },
+
+    {
+        "name": "Australie",
+        "category_name": "Oceanie",
+        "category_key": "oceania",
+        "url": "country.html?p=astralia",
+        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni iure sunt cum ullam facilis quae. Totam, nulla, porro asperiores numquam atque unde maiores quidem earum, dicta cupiditate dolorum harum eius?",
+        "picture": "images/expatriation-australie.jpg"
+
+    },
+
+    {
+        "name": "Sénégal",
+        "category_name": "Afrique",
+        "category_key": "africa",
+        "url": "country.html?p=seneral",
+        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni iure sunt cum ullam facilis quae. Totam, nulla, porro asperiores numquam atque unde maiores quidem earum, dicta cupiditate dolorum harum eius?",
+        "picture": "images/pirogues-de-peche-senegal-479771-1440x720.jpg"
+
+    }
+    
+
+
+]
